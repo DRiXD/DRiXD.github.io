a3c4b52d32e44ec4ec55b85dfc21a62b3de200d8
denisrei <39619011+DRiXD@users.noreply.github.com>
2023-08-31 20:29:27 +0000

    set fixed order for javascripts

--- responsive.js
+++ responsive.js
@@ -1,33 +1,32 @@
     function adjustTextSizeAndSpacing() {
         const loremTextDiv = document.querySelector('.lorem-text');
         const introTextDiv = document.querySelector('.intro-text');
-        const polaroids_1 =  $('> *', $('.polaroid-container1'));;
-
+        const polaroids =  document.querySelector('.polaroid');
         
         if (window.innerWidth <= 600 || window.innerHeight <= 600) {
             loremTextDiv.style.fontSize = '16px';
             loremTextDiv.style.lineHeight = '1.2';
             introTextDiv.style.fontSize = '30px';
-            polaroids_1.style.width = '200px';
-            polaroids_1.style.height = '250px';
-            polaroids_1.style.fontSize = '16px';
+            polaroids.style.width = '200px';
+            polaroids.style.height = '250px';
+            polaroids.style.fontSize = '16px';
     
         } else if ((window.innerWidth > 600 && window.innerWidth <= 900) || 
                    (window.innerHeight > 600 && window.innerHeight <= 900)) {
             loremTextDiv.style.fontSize = '18px';
             loremTextDiv.style.lineHeight = '1.4';
             introTextDiv.style.fontSize = '40px';
-            polaroids_1.style.width = '250px';
-            polaroids_1.style.height = '300px';
-            polaroid_1.style.fontSize = '17px';
+            polaroids.style.width = '250px';
+            polaroids.style.height = '300px';
+            polaroids.style.fontSize = '17px';
 
         } else {
             loremTextDiv.style.fontSize = '22px';
             loremTextDiv.style.lineHeight = '1.6';
             introTextDiv.style.fontSize = '50px';
-            polaroids_1.style.width = '300px';
-            polaroids_1.style.height = '350px';
-            polaroids_1.style.fontSize = '18px';
+            polaroids.style.width = '300px';
+            polaroids.style.height = '350px';
+            polaroids.style.fontSize = '18px';
 
         }
     }
